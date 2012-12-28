fc4f3a505571ee821229d45925dfb2651f39e859
Oleg Pisakin <pisakin@gmail.com>
2012-12-27 21:15:55 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+import sys; 
+import os;
+
+#Импортируем библиотеку Math
+import math
+#Импортируем один из пакетов Matplotlib
+import pylab
+#Импортируем пакет со вспомогательными функциями
+from matplotlib import mlab
+
+####
+if __name__ == "__main__":
+  print("test");
+    #app.run()
